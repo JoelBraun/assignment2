45bfcd41a9bbdc1e2fc697acdbe2d7e04a8ff8db
gunit367 <gunit367@gmail.com>
2014-10-25 16:46:25 -0700

    Added initial draw functions
    Still need to add pause button but this is the basic rendering of the
    grid.

--- assignment2.rkt
+++ assignment2.rkt
@@ -8,16 +8,67 @@
 
 ;Initial function definitions
 (define (tickfn w) ...)
-(define (drawfn w) ...)
+
+
+;Draw Function
+; Draws 8x8 grid with squares filled in marked by the world
+; world -> image
+
+; Function for empty/filled button
+(define (button filled?)
+  (if filled? (overlay (square 73 'solid 'red)
+                       (square 75 'solid 'black))
+      (square 75 'outline 'black)))
+
+
+; world -> image
+(define (renderfn world)
+  (place-image/align (drawfn (world-grid world))
+                     0 0
+                     "left" "top"
+                     (empty-scene 1000 (* 75 9))))
+
+
+; grid -> image
+(define (drawfn grid) 
+  (above (colrender (grid-c0 grid))
+         (colrender (grid-c1 grid))
+         (colrender (grid-c2 grid))
+         (colrender (grid-c3 grid))
+         (colrender (grid-c4 grid))
+         (colrender (grid-c5 grid))
+         (colrender (grid-c6 grid))
+         (colrender (grid-c7 grid))))
+
+
+; List -> image
+(define (colrender collist) 
+  (beside (button (list-ref collist 0))
+          (button (list-ref collist 1))
+          (button (list-ref collist 2))
+          (button (list-ref collist 3))
+          (button (list-ref collist 4))
+          (button (list-ref collist 5))
+          (button (list-ref collist 6))
+          (button (list-ref collist 7))))
+
+
+
 (define (mousefn w x y evt) ...)
   
-(define-struct world (grid col tick))
-(define-struct col (s0 s1 s2 s3 s4 s5 s6 s7))
+(define-struct world (grid col tick tempo playing?))
+
+
 (define-struct grid (c0 c1 c2 c3 c4 c5 c6 c7))
-(define INITIAL (make-world (make-grid INITIAL_COLUMN INITIAL_COLUMN INITIAL_COLUMN INITIAL_COLUMN INITIAL_COLUMN INITIAL_COLUMN INITIAL_COLUMN INITIAL_COLUMN) INITIAL_COLUMN 0))
-(define INITIAL_INITIAL_COLUMN (make-col 0 0 0 0 0 0 0 0 ))
+
+(define INITIAL_COLUMN (list false false false false false false false false))
+(define INITIAL (make-world (make-grid INITIAL_COLUMN INITIAL_COLUMN INITIAL_COLUMN INITIAL_COLUMN
+                                       INITIAL_COLUMN INITIAL_COLUMN INITIAL_COLUMN INITIAL_COLUMN)
+                            INITIAL_COLUMN 0 1 true))
+
+
 (big-bang INITIAL
-          [to-draw drawfn]
+          [to-draw renderfn]
           [on-mouse mousefn]
           [on-tick tickfn]
           )
